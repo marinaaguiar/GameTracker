0f4ff728428dab72ad3a247a5a66194f4c91122f
Marina Aguiar <gordinho.marina@gmail.com>
2022-09-15 17:39:35 +0100

    wip layout

---
+++ GameTracker/Common/CompositionalLayout.swift
@@ -0,0 +1,86 @@
+import Foundation
+import UIKit
+
+enum CompositionalGroupAligment {
+    case vertical
+    case horizontal
+}
+
+struct CompositionalLayout {
+
+    static func createItem(
+        width: NSCollectionLayoutDimension,
+        height: NSCollectionLayoutDimension,
+        spacing: CGFloat
+    ) -> NSCollectionLayoutItem {
+
+        let item = NSCollectionLayoutItem(
+            layoutSize:
+                NSCollectionLayoutSize(
+                    widthDimension: width,
+                    heightDimension: height)
+        )
+        item.contentInsets = NSDirectionalEdgeInsets(
+            top: spacing,
+            leading: spacing,
+            bottom: spacing,
+            trailing: spacing
+        )
+        return item
+    }
+
+    static func createGroup(
+        aligment: CompositionalGroupAligment,
+        width: NSCollectionLayoutDimension,
+        height: NSCollectionLayoutDimension,
+        items: [NSCollectionLayoutItem]
+    ) -> NSCollectionLayoutGroup {
+
+        switch aligment {
+
+        case .vertical:
+            return NSCollectionLayoutGroup.vertical(
+                layoutSize:
+                    NSCollectionLayoutSize(
+                        widthDimension: width,
+                        heightDimension: height),
+                subitems: items)
+        case .horizontal:
+            return NSCollectionLayoutGroup.horizontal(
+                layoutSize:
+                    NSCollectionLayoutSize(
+                        widthDimension: width,
+                        heightDimension: height),
+                subitems: items)
+        }
+    }
+
+    static func createGroup(
+        aligment: CompositionalGroupAligment,
+        width: NSCollectionLayoutDimension,
+        height: NSCollectionLayoutDimension,
+        item: NSCollectionLayoutItem,
+        count: Int
+    ) -> NSCollectionLayoutGroup {
+
+        switch aligment {
+
+        case .vertical:
+            return NSCollectionLayoutGroup.vertical(
+                layoutSize:
+                    NSCollectionLayoutSize(
+                        widthDimension: width,
+                        heightDimension: height),
+                subitem: item,
+                count: count)
+        case .horizontal:
+            return NSCollectionLayoutGroup.horizontal(
+                layoutSize:
+                    NSCollectionLayoutSize(
+                        widthDimension: width,
+                        heightDimension: height),
+                subitem: item,
+                count: count)
+        }
+    }
+}
--- GameTracker/Scenes/ExplorerViewController/ExplorerViewController.swift
+++ GameTracker/Scenes/ExplorerViewController/ExplorerViewController.swift
@@ -1,10 +1,182 @@
 import UIKit
 
+
+enum Section: Int, CaseIterable {
+    case topRanked = 0
+    case popularGames = 1
+    case levelOfComplexity = 2
+    case mechanics = 3
+    case numberOfPlayers = 4
+
+    var sectionTitle: String {
+        switch self {
+        case .topRanked:
+            return "Top Ranked"
+        case .popularGames:
+            return "Popular Games"
+        case .levelOfComplexity:
+            return "LevelOfComplexity"
+        case .mechanics:
+            return "Mechanics"
+        case .numberOfPlayers:
+            return "Number Of Players"
+        }
+    }
+}
+
 class ExplorerViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCollectionView()
 
-        // Do any additional setup after loading the view.
     }
+
+    func setupCollectionView() {
+        collectionView.register(
+            ExplorerCollectionViewCell.self, forCellWithReuseIdentifier: ExplorerCollectionViewCell.identifier
+        )
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.collectionViewLayout = createLayout()
+        collectionView.allowsMultipleSelection = true
+    }
+
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment in
+            let section = Section(rawValue: sectionIndex)
+
+            switch section {
+            case .topRanked:
+                return self.createMediumSizeTableSection()
+            case .popularGames:
+                return self.createBigSizeTableSection()
+            case .levelOfComplexity:
+                return self.createSmallSizeTableSection()
+            case .mechanics:
+                return self.createMediumSizeTableSection()
+            case .numberOfPlayers:
+                return self.createSmallSizeTableSection()
+            case .none:
+                return self.createMediumSizeTableSection()
+            }
+
+        }
+
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 5
+        layout.configuration = config
+        return layout
+
+    }
+
+    func createBigSizeTableSection() -> NSCollectionLayoutSection {
+        let item = CompositionalLayout.createItem(
+            width: .fractionalWidth(1),
+            height: .fractionalHeight(1),
+            spacing: 5)
+        let horizontalGroup = CompositionalLayout.createGroup(
+            aligment: .horizontal,
+            width: .fractionalWidth(2.8/3),
+            height: .fractionalHeight(1/3),
+            item: item,
+            count: 1)
+        let section = NSCollectionLayoutSection(group: horizontalGroup)
+        section.orthogonalScrollingBehavior = .groupPaging
+
+        let sectionHeader = createSectionHeader()
+        section.boundarySupplementaryItems = [sectionHeader]
+
+        return section
+    }
+
+    func createMediumSizeTableSection() -> NSCollectionLayoutSection {
+        let item = CompositionalLayout.createItem(
+            width: .fractionalWidth(1),
+            height: .fractionalHeight(1),
+            spacing: 5)
+        let horizontalGroup = CompositionalLayout.createGroup(
+            aligment: .horizontal,
+            width: .fractionalWidth(2/3),
+            height: .fractionalHeight(1/5),
+            item: item,
+            count: 1)
+        let section = NSCollectionLayoutSection(group: horizontalGroup)
+        section.orthogonalScrollingBehavior = .continuous
+
+        let sectionHeader = createSectionHeader()
+        section.boundarySupplementaryItems = [sectionHeader]
+
+        return section
+    }
+
+    func createSmallSizeTableSection() -> NSCollectionLayoutSection {
+        let item = CompositionalLayout.createItem(
+            width: .fractionalWidth(1),
+            height: .fractionalHeight(1),
+            spacing: 10)
+        let horizontalGroup = CompositionalLayout.createGroup(
+            aligment: .horizontal,
+            width: .absolute(180),
+            height: .absolute(60),
+            item: item,
+            count: 1)
+        let section = NSCollectionLayoutSection(group: horizontalGroup)
+        section.orthogonalScrollingBehavior = .continuous
+
+        let sectionHeader = createSectionHeader()
+        section.boundarySupplementaryItems = [sectionHeader]
+
+        return section
+    }
+
+    func createSectionHeader() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let sectionHeaderSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.93), heightDimension: .estimated(30))
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: sectionHeaderSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        return sectionHeader
+    }
+}
+
+//MARK: - UICollectionViewDataSource
+
+extension ExplorerViewController: UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return Section.allCases.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        let numberOfItemsInSection = Section(rawValue: section)
+
+        switch numberOfItemsInSection {
+        case .topRanked:
+            return 3
+        case .popularGames:
+            return 4
+        case .levelOfComplexity:
+            return 5
+        case .mechanics:
+            return 10
+        case .numberOfPlayers:
+            return 6
+        case .none:
+            return 0
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let explorerCell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ExplorerCollectionViewCell.identifier,
+            for: indexPath
+        )
+        return explorerCell
+    }
+
+}
+
+extension ExplorerViewController: UICollectionViewDelegate {
+
 }
